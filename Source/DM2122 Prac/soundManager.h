abadf1bebb2ff4b14ea9b112aa98df6be2b4228c
pek <195094J@mymail.nyp.edu.sg>
2020-02-21 21:31:27 +0800

    added sound

---
+++ Source/DM2122 Prac/soundManager.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "../irrklang/include/irrKlang.h"
+#include <string>
+using namespace std;
+
+class soundManager
+{
+private:
+	float posX;
+	float posY;
+	float posZ;
+	string addSound;
+
+public:
+	soundManager();
+	~soundManager();
+	void play2DSound(string sound);
+	void play3DSound(string sound, float posX, float posY, float posZ);
+	
+	float getPosX();
+	void setPosX(float X);
+	float getPosY();
+	void setPosY(float Y);
+	float getPosZ();
+	void setPosZ(float Z);
+};
