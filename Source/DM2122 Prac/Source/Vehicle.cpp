6ca9e7e7214385115a6023a9d85abe9315fb667f
pek <195094J@mymail.nyp.edu.sg>
2020-02-25 04:29:15 +0800

    iwanttodie

--- Source/DM2122 Prac/Source/Vehicle.cpp
+++ Source/DM2122 Prac/Source/Vehicle.cpp
@@ -97,11 +97,11 @@ physics* Vehicle::getRB()
 
 void Vehicle::updatePos()
 {
+	position.x += this->RB.getForce().x;
+	position.z += this->RB.getForce().z;
+	position.y += this->RB.getForce().y;
 
 	Vector3 currentPos = this->getPos();
-	position.x += this->RB.getVelo().x;
-	position.z += this->RB.getVelo().z;
-	position.y += this->RB.getVelo().y;
 	this->setPos(currentPos.x, currentPos.y, currentPos.z);
 }
 
